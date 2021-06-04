303db75da14f6859d0db654b86d7dc2f94ae04d4
Kibwe Howard <kbwhwd@gmail.com>
2021-06-03 20:43:02 -0400

    First commit for this project.

---
+++ app.py
@@ -0,0 +1,20 @@
+import pprint
+import wikipedia
+from gtts import gTTS
+from bs4 import BeautifulSoup
+from playsound import playsound
+
+try:
+    while True:
+        q = wikipedia.search(str(input("Search Leafhopper: ")))
+        r = wikipedia.summary(q[0], sentences=0, chars=0, auto_suggest=False, redirect=False)
+        s = BeautifulSoup(r, 'html.parser')
+        pprint.pprint(q[0])
+        pprint.pprint(s.prettify().replace('\n', ''))
+        v = gTTS(str(s))
+        v.save('temp.mp3')
+        playsound('temp.mp3')
+
+except wikipedia.exceptions.DisambiguationError as error:
+    print(error.options)
+    pass
